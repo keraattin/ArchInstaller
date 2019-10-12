dcf1fdb29362b3da57df24b4ed71daec9aa570ae
keraattin <kerem-oruc@outlook.com>
2019-10-12 20:00:30 +0300

    Manual disk partitioning

--- arch_installer.py
+++ arch_installer.py
@@ -3,12 +3,15 @@
 import os
 import sys
 
+
+#Colors
 DEFAULT = "\33[37m"
 GREEN = "\33[32m"
 RED = "\33[31m"
 
+#Welcome message function
 def welcome():
-    print(GREEN)
+    print(GREEN)    #For green output format
     print(" █████╗ ██████╗  ██████╗██╗  ██╗")
     print("██╔══██╗██╔══██╗██╔════╝██║  ██║")
     print("███████║██████╔╝██║     ███████║")
@@ -22,17 +25,30 @@ def welcome():
     print("██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     ██╔══╝  ██╔══██╗")
     print("██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗███████╗██║  ██║")
     print("╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝")
-    print(DEFAULT)
+    print(DEFAULT)  #For default output format
 
     to_continue = input("Press any key to continue..")
-    os.system('clear')
+    os.system("clear")
+
+#Disk partitioning function
+def disk_partitioning():
+    os.system("cfdisk")
+    os.system("clear")
+    os.system("lsblk")
+    response = input("Are you sure about this configuration? [Y/n]")
+    if response == 'Y' or response == 'y' or response == '':
+        os.system("clear")
+        return
+    else:
+        disk_partitioning()
 
 
 def main():
-    if os.geteuid() != 0:
+    if os.geteuid() != 0:   #Check whether user have root privileges or not
         print(RED+"You are not root"+DEFAULT)
         sys.exit(1)
     welcome()
+    disk_partitioning()
 
 if __name__ == '__main__':
     main()

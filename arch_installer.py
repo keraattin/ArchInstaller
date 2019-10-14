bc9ce8558f4bb382730d038b328c2acac23c31da
keraattin <kerem-oruc@outlook.com>
2019-10-14 22:58:27 +0300

    String splitting tests

--- arch_installer.py
+++ arch_installer.py
@@ -30,11 +30,11 @@ def welcome():
     to_continue = input("Press any key to continue..")
     os.system("clear")
 
-
-def manuel_partitioning():
-    os.system("cfdisk")
+#Manual partitioning
+def manual_partitioning():
+    os.system("cfdisk") #For manual configuration
     os.system("clear")
-    os.system("lsblk")
+    os.system("lsblk")  #Printing out patitions for checking configuration
     response = input("Are you sure about this configuration? [Y/n]")
     if response == 'Y' or response == 'y' or response == '':
         os.system("clear")
@@ -44,10 +44,43 @@ def manuel_partitioning():
 
 
 def uefi_partitioning():
-    os.system("parted /dev/sda mklabel gpt --script")
+    efi_size = input("Insert EFI partition size [M/MiB - G/GiB] : ")
+    if efi_size.find('M') != -1 or efi_size.find('MiB') != -1:
+        found_point = efi_size.find('M')
+        efi_part_size = efi_size[:found_point]
+        efi_part_type = efi_size[found_point:]
+    elif efi_size.find('G') != -1 or efi_size.find('GiB') != -1:
+        found_point = efi_size.find('G')
+        efi_part_size = efi_size[:found_point]
+        efi_part_type = efi_size[found_point:]
+    else:
+        print(RED+"Wrong format entered.\nEnter size like 100M or 100MiB"+DEFAULT)
+        uefi_partitioning() #If wrong format entered, call again itself
+    boot_size = input("Insert /boot partition size [M/MiB - G/GiB] : ")
+    if boot_size.find('M') != -1 or boot_size.find('MiB') != -1:
+        found_point = boot_size.find('M')
+        boot_part_size = boot_size[:found_point]
+        boot_part_type = boot_size[found_point:]
+    elif boot_size.find('G') != -1 or boot_size.find('GiB') != -1:
+        found_point = boot_size.find('G')
+        boot_part_size = boot_size[:found_point]
+        boot_part_type = boot_size[found_point:]
+    else:
+        print(RED+"Wrong format entered.\nEnter size like 100M or 100MiB"+DEFAULT)
+        uefi_partitioning() #If wrong format entered, call again itself
+    swap_size = input("Insert swap partition size [M/MiB - G/GiB] : ")
+
+    print("efi_part_size : "+str(efi_part_size))
+    print("efi_part_type : "+str(efi_part_type))
+    print("boot_part_size : "+str(boot_part_size))
+    print("boot_part_type : "+str(boot_part_type))
+    #os.system("parted /dev/sda mklabel gpt --script") #Making label to gpt
+    #os.system("parted /dev/sda mkpart primary fat32 1M {} set 1 esp on --script".format(efi_size))
+    #os.system("parted /dev/sda mkpart primary ext2 {} {} --script".format(efi_size,)
 
 def dos_partitioning():
-    os.system("parted /dev/sda mklabel msdos --script")
+    #os.system("parted /dev/sda mklabel msdos --script") #Making label to msdos
+    print("auto")
 
 def auto_partitioning():
     selection = input("1-UEFI (new) ,2-DOS (old) \nSelection [1/2] :")
@@ -58,15 +91,16 @@ def auto_partitioning():
     else:
         os.system("clear")
         print(RED+"Wrong Selection"+DEFAULT)
+        auto_partitioning()
 
 
 #Disk partitioning function
 def disk_partitioning():
-    selection = input("1-Auto Partitioning, 2-Manuel Partitioning\nSelection [1/2] : ")
+    selection = input("1-Auto Partitioning, 2-Manual Partitioning\nSelection [1/2] : ")
     if selection == '1':
         auto_partitioning()
     elif selection == '2':
-        manuel_partitioning()
+        manual_partitioning()
 
 
 

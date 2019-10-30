240f703efdcb7645134ceb0f7bbd701e6876d350
keraattin <kerem-oruc@outlook.com>
2019-10-30 21:41:43 +0300

    #3 root password set method added

--- inside_installer.py
+++ inside_installer.py
@@ -8,3 +8,9 @@ HOSTNAME = ""
 USERNAME = ""
 PASSWORD = ""
 ROOT_PASSWORD = ""
+
+#Setting new password for root
+def set_root_password():
+    os.system("clear")
+    root_password = input("\nPlease enter the root password")
+    os.system('echo -e {}\n{} | passwd root'.format(root_password,root_password))
